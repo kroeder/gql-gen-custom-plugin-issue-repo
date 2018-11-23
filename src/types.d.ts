4f32cf09dedcaf28d194f555c2b3377f7e431175
kro <kai.roeder@aeb.com>
2018-11-23 11:37:12 +0100

    initial commit

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+/** A list of options for the sort order of the feed */
+export enum FeedType {
+  HOT = "HOT",
+  NEW = "NEW",
+  TOP = "TOP"
+}
+/** The type of vote to record, when submitting a vote */
+export enum VoteType {
+  UP = "UP",
+  DOWN = "DOWN",
+  CANCEL = "CANCEL"
+}
